818059993d7281932842fc04f94ba4a6d1dde8cf
RUDRA MADHAB DHAL <rudramadhabdhal@gmail.com>
2025-12-01 00:03:23 +0530

    Add backend code: user auth, profile, logout, and docs

--- models/user.model.js
+++ models/user.model.js
@@ -4,48 +4,29 @@ const jwt = require('jsonwebtoken');
 
 const userSchema = new mongoose.Schema({
     fullname: {
-        firstname: {
-            type: String,
-            required: true,
-            minlength: [3, 'First name must be at least 3 characters long'],
-        },
-        lastname: {
-            type: String,
-            minlength: [3, 'Last name must be at least 3 characters long'],
-        }
+        firstname: { type: String, required: true, minlength: 3 },
+        lastname: { type: String, minlength: 3 }
     },
-    email: {
-        type: String,
-        required: true,
-        unique: true,
-        minlength: [5, 'Email must be at least 5 characters long'],
-    },
-    password: {
-        type: String,
-        required: true,
-        select: false,
-    },
-    socketId: {
-        type: String,
-    }
+    email: { type: String, required: true, unique: true, minlength: 5 },
+    password: { type: String, required: true, select: false },
+    socketId: { type: String }
 });
 
-// Method to generate JWT token
+// Instance method to generate JWT token
 userSchema.methods.generateAuthToken = function() {
-    const token = jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    return token;
+    return jwt.sign({ _id: this._id }, process.env.JWT_SECRET, { expiresIn: '24h' });
 }
 
-// Method to compare password
+// Instance method to compare passwords
 userSchema.methods.comparePassword = async function(password) {
-    return await bcrypt.compare(password, this.password);
+    return bcrypt.compare(password, this.password);
 }
 
 // Static method to hash password
 userSchema.statics.hashPassword = async function(password) {
-    return await bcrypt.hash(password, 10);
+    return bcrypt.hash(password, 10);
 }
 
-const userModel = mongoose.model('user', userSchema);
+const userModel = mongoose.model('User', userSchema);
 
 module.exports = userModel;
